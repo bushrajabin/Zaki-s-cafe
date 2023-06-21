f9ef865db23c0814be144d761c5f57e504c1e632
Bushra jabin <bushrabintjabin@gmail.com>
2023-06-21 12:26:36 +0530

    Css

---
+++ script.js
@@ -0,0 +1,10 @@
+function showLinks(){
+    document.getElementById("links2").style.display="flex";
+  
+}
+
+
+function closeLinks(){
+    document.getElementById("links2").style.display="none";
+}
+// showLinks();
