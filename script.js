63660cc6360c1b565ab3c6f1019bef363ea848b1
Bushra jabin <bushrabintjabin@gmail.com>
2023-06-21 23:26:16 +0530

    changeIn HTML& CSS & JS

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function closeLinks() {
 function showHome() {
     document.getElementById("links2").style.display = "none"
 }
+
+
+function showMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
